0802e29b08a0e6aea1bdbc2f63ade1e0c77010ab
Joel Ross <joelross@uw.edu>
2016-05-18 10:23:25 -0700

    End of lecture code

--- script.js
+++ script.js
@@ -2,4 +2,20 @@
 
 var names = ['Alice', 'Bob', 'Charles', 'Debbie', 'Edward', 'Fred'];
 
+$('#text').click(function(event){
+	console.log("clicky clicky");
+	$('#text').append("etc. ");	
+})
+
+$('li').click(function(event){
+	var element = $(event.target);
+	console.log("You clicked "+element.html());	
+	element.fadeOut(500);
+});
+
+var url = "http://data.unhcr.org/api/stats/persons_of_concern.json?year=2013&country_of_residence=USA"
+
+$.get(url).then(function(data){
+	console.log(data);
+})
 
